fa618d259cff981b2484f26fc49bdd3a029cde0a
Francisco Erlyson Pamplona <77758434+DreamEater360@users.noreply.github.com>
2023-12-07 15:07:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ right_motor = Motor(Port.A)
 left_motor = Motor(Port.D)
 CSenRight = ColorSensor(Port.S3)
 CSenLeft = ColorSensor(Port.S2)
+UltSen = UltrasonicSensor(Port.S1)
 
 MOV = DriveBase(left_motor, right_motor, wheel_diameter=40, axle_track=135)
 
@@ -24,6 +25,14 @@ Kp = 3.5  # const P
 Ki = 0.1  # const I
 Kd = 0.1  # const D
 speed = 70 # Velocidade em mm/s
+
+# tamanho dos obstaculos
+# pequeno
+CP = 100/2
+LP = 100/2
+# grande
+CG = 100/2
+LG = 100/2
 
 I = 0
 P_ant = 0
@@ -48,13 +57,56 @@ def VirarLeft(MOV):
     MOV.turn(-90)
     MOV.straight(50)
 
+def OBS_P_Right(MOV, CP, LP):
+    MOV.stop()
+    MOV.turn(90)
+    MOV.straight(LP)
+    MOV.turn(-90)
+    MOV.straight(CP)
+    MOV.turn(-90)
+    MOV.straight(LP)
+    MOV.turn(90)
+
+def OBS_P_Left(MOV, CP, LP):
+    MOV.stop()
+    MOV.turn(-90)
+    MOV.straight(LP)
+    MOV.turn(90)
+    MOV.straight(CP)
+    MOV.turn(90)
+    MOV.straight(LP)
+    MOV.turn(-90)
+
+def OBS_G_Right(MOV, CG, LG):
+    MOV.stop()
+    MOV.turn(90)
+    MOV.straight(LG)
+    MOV.turn(-90)
+    MOV.straight(CG)
+    MOV.turn(-90)
+    MOV.straight(LG)
+    MOV.turn(90)
+
+def OBS_G_Left(MOV, CG, LG):
+    MOV.stop()
+    MOV.turn(-90)
+    MOV.straight(LG)
+    MOV.turn(90)
+    MOV.straight(CG)
+    MOV.turn(90)
+    MOV.straight(LG)
+    MOV.turn(-90)
+
 while True:
     RcorLeft = CSenRight.reflection()
     RcorRight = CSenRight.reflection()
 
     P = RcorLeft - RcorRight
 
-    if CSenRight.color() == color.GREEN and CSenLeft.color() == color.GREEN:
+    if UltSen.distance() >= 100:
+        OBS_P_Right(MOV, CP, LP)
+        PID(I, Kp, Ki, Kd, speed, MOV, P_ant)
+    elif CSenRight.color() == color.GREEN and CSenLeft.color() == color.GREEN:
         PID(I, Kp, Ki, Kd, speed, MOV, P_ant)
     elif CSenLeft.color() == color.GREEN:
         VirarLeft(MOV)
