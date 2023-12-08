a711d1e8146190121ca9636e32205e9592fb5455
DreamEater <erlysonpamplona1234@gmail.com>
2023-12-08 10:59:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
+from pybricks.robotics import DriveBaseS
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -19,6 +19,9 @@ CSenRight = ColorSensor(Port.S3)
 CSenLeft = ColorSensor(Port.S2)
 UltSen = UltrasonicSensor(Port.S1)
 
+girar_garra = Motor(Port.B)
+fechar_garra = Motor(Port.C)
+
 MOV = DriveBase(left_motor, right_motor, wheel_diameter=40, axle_track=135)
 
 Kp = 3.5  # const P
@@ -27,12 +30,8 @@ Kd = 0.1  # const D
 speed = 70 # Velocidade em mm/s
 
 # tamanho dos obstaculos
-# pequeno
-CP = 100/2
-LP = 100/2
-# grande
-CG = 100/2
-LG = 100/2
+Com = 100/2
+Lar = 100/2
 
 I = 0
 P_ant = 0
@@ -57,46 +56,28 @@ def VirarLeft(MOV):
     MOV.turn(-90)
     MOV.straight(50)
 
-def OBS_P_Right(MOV, CP, LP):
+def OBS_Right(MOV, Com, Lar):
     MOV.stop()
     MOV.turn(90)
-    MOV.straight(LP)
+    MOV.straight(Lar)
     MOV.turn(-90)
-    MOV.straight(CP)
+    MOV.straight(Com)
     MOV.turn(-90)
-    MOV.straight(LP)
+    MOV.straight(Lar)
     MOV.turn(90)
-
-def OBS_P_Left(MOV, CP, LP):
-    MOV.stop()
-    MOV.turn(-90)
-    MOV.straight(LP)
-    MOV.turn(90)
-    MOV.straight(CP)
-    MOV.turn(90)
-    MOV.straight(LP)
-    MOV.turn(-90)
-
-def OBS_G_Right(MOV, CG, LG):
-    MOV.stop()
-    MOV.turn(90)
-    MOV.straight(LG)
-    MOV.turn(-90)
-    MOV.straight(CG)
-    MOV.turn(-90)
-    MOV.straight(LG)
-    MOV.turn(90)
-
-def OBS_G_Left(MOV, CG, LG):
-    MOV.stop()
-    MOV.turn(-90)
-    MOV.straight(LG)
-    MOV.turn(90)
-    MOV.straight(CG)
-    MOV.turn(90)
-    MOV.straight(LG)
-    MOV.turn(-90)
-
+    
+def Resgate(MOV, UltSen, girar_garra, fechar_garra):
+    if UltSen.distance() >= 100:
+        MOV.drive(70, 0)
+    else:
+        MOV.stop()
+        girar_garra.run_target(50, -180, then=Stop.HOLD, wait=True)
+        MOV.straight(100)
+        fechar_garra.run_time(50, 3, then=Stop.HOLD, wait=True)
+        girar_garra.run_target(50, 180, then=Stop.HOLD, wait=True)
+        MOV.straight(-50)
+        MOV.turn(90)
+        
 while True:
     RcorLeft = CSenRight.reflection()
     RcorRight = CSenRight.reflection()
@@ -104,7 +85,7 @@ while True:
     P = RcorLeft - RcorRight
 
     if UltSen.distance() >= 100:
-        OBS_P_Right(MOV, CP, LP)
+        OBS_Right(MOV, Com, Lar)
         PID(I, Kp, Ki, Kd, speed, MOV, P_ant)
     elif CSenRight.color() == color.GREEN and CSenLeft.color() == color.GREEN:
         PID(I, Kp, Ki, Kd, speed, MOV, P_ant)
